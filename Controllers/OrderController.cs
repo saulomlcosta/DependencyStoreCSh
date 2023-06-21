55ed48b4a85ab3bc3de7e05e821a11adf6cc0697
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2023-06-21 14:45:25 -0300

    Ajuste na controladora.

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -31,19 +31,8 @@ public class OrderController : ControllerBase
         var deliveryFee = await _deliveryFeeService.GetDeliveryFeeAsync(zipCode);
         var coupon = await _promoCodeRepository.GetPromoCodeAsync(promoCode);
         var discount = coupon?.Value ?? 0M;
-
-        decimal subTotal = 0;
-        const string getProductQuery = "SELECT [Id], [Name], [Price] FROM PRODUCT WHERE ID=@id";
-        for (var p = 0; p < products.Length; p++)
-        {
-            Product product;
-            await using (var conn = new SqlConnection("CONN_STRING"))
-                product = await conn.QueryFirstAsync<Product>(getProductQuery, new { Id = p });
-
-            subTotal += product.Price;
-        }
-
         var order = new Order(deliveryFee, discount, new List<Product>());
+
         return Ok($"Pedido {order.Code} gerado com sucesso!");
     }
 }
